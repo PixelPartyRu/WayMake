dd922c5ea921c8fedf8e861e3688329a4aec653a
mitya <m.nenilin@avia-centr.ru>
2016-10-19 15:56:20 +0300

    Добавление моделей и миграций
    Статусы пользователей

---
+++ Portal/database/migrations/2016_10_19_121900_add_filds_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFildsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('second_name');
+            $table->string('third_name');
+            $table->integer('id_user_type')->unsigned();
+            $table->foreign('id_user_type')->references('id')->on('user_types')->onDelete('cascade');  
+});
+
+
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
