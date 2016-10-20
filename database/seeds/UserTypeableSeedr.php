4ca3728f0d12816dbb7554eef62feac592bb9044
mitya <m.nenilin@avia-centr.ru>
2016-10-20 09:28:14 +0300

    Добавлн seeder
    Добавлено добавление стандартного пользователя в таблицу ттипы пользователей

--- database/seeds/UserTypeableSeedr.php
+++ database/seeds/UserTypeableSeedr.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserTypeableSeedr extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(UserTypeableSeedr::class);
+
+    	DB::table('user_types')->insert(['user_type' => 'пользователь']);
+        //
     }
 }
--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -536,4 +536,5 @@ return array(
     'SebastianBergmann\\Version' => $vendorDir . '/sebastian/version/src/Version.php',
     'TestCase' => $baseDir . '/tests/TestCase.php',
     'Text_Template' => $vendorDir . '/phpunit/php-text-template/src/Template.php',
+    'UserTypeableSeedr' => $baseDir . '/database/seeds/UserTypeableSeedr.php',
 );
