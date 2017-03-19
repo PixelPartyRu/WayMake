c71eb3d1b1e89e0c2537e55f5077feb8821407b8
Solovyan Mikhail <hello@pixelparty.ru>
2017-03-19 23:40:58 +0300

    fix migrations

--- database/migrations/2016_10_19_121900_add_filds_to_users_table.php
+++ database/migrations/2016_10_19_121900_add_filds_to_users_table.php
@@ -16,13 +16,10 @@ class AddFildsToUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('second_name');
             $table->string('third_name');
-            $table->integer('id_user_type')->unsigned();
-            $table->foreign('id_user_type')->references('id')->on('user_types')->onDelete('cascade');  
-});
-
-
+            $table->integer('user_type_id')->unsigned();
+            $table->foreign('user_type_id')->references('id')->on('user_types')->onDelete('cascade');  
+        });
     }
-
 
     /**
      * Reverse the migrations.
@@ -32,9 +29,8 @@ class AddFildsToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-                    $table->dropForeign(['id_user_type']);  
-                   $table->dropColumn(['second_name', 'third_name', 'id_user_type']);
-});
-        //
+            $table->dropForeign(['id_user_type']);
+            $table->dropColumn(['second_name', 'third_name', 'user_type_id']);
+        });
     }
 }
